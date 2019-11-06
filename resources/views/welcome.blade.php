39fafd962793b52292029677449cb5e86d1495f8
Sinh00 <msoirnthe@gmail.com>
2019-11-06 11:13:44 +0900

    update text
    developブランチ用のテキストに変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelのdevelopブランチです。
                 </div>
 
                 <div class="links">
