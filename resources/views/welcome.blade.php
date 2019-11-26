c3f9eafe9ee8e48de411fd7b8fd5f610ad0a3798
Sinh00 <msoirnthe@gmail.com>
2019-11-26 09:00:40 +0900

    update text
    テキストを修正しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello, Laravel
+                    Hello, Laravel?
                 </div>
                 <p>こんにちは、Laravel！</p>
 
