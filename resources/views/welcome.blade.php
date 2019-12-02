fe8e6f9ea1670155b5c97c25424ffafd33fda290
Sinh00 <msoirnthe@gmail.com>
2019-12-02 13:04:24 +0900

    delte subtext
    サブテキストを削除しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,6 @@
                 <div class="title m-b-md">
                     Hello, Laravel!
                 </div>
-                <p>こんにちは、Laravel！</p>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
